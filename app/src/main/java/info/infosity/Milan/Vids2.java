a016da0f1277a9e45bea7b9ff7842d967a7ab862
agb91 <andrea.damio1991@hotmail.it>
2016-03-19 10:17:06 +0100

    source

---
+++ app/src/main/java/info/infosity/Milan/Vids2.java
@@ -0,0 +1,24 @@
+package info.infosity.Milan;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import android.text.format.Time;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+
+public class Vids2 extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+    }
+}
